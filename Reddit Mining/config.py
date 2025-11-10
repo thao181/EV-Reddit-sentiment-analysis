f1f63d22f73d36527be014e428c76f9fb7f5e83a
Phuong Thao Le <148855428+thao181@users.noreply.github.com>
2025-11-11 00:51:32 +1100

    Add files via upload

---
+++ Reddit Mining/config.py
@@ -0,0 +1,6 @@
+# config.py
+
+# Thông tin cấu hình API Reddit
+CLIENT_ID = "RVt5ZKyOHzPiBpIk1jFygQ"
+CLIENT_SECRET = "omMlRhfEaZCQlERnFec5W-gNkpaSGQ"
+USER_AGENT = "RedditMiner/0.1 by HomeSignificant4579"
